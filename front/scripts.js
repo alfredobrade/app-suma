dfc4677d28ed9d3a30542126d3efa7b2aa7ecd4f
Alfredo Brade <alfredobrade@gmail.com>
2023-05-03 12:26:24 -0300

    cambio de los scripts por Eric

--- front/scripts.js
+++ front/scripts.js
@@ -5,26 +5,43 @@ const result = document.getElementById("result")
 
 
 
-boton.addEventListener("click", e => {
-    
+boton.addEventListener("mousedown", async () => {
+
+    try {
+        //const re = (Number(num1.value)+Number(num2.value))
+        const data = {
+            Np: Number(num1.value),
+            Ns: Number(num2.value),
+            Resultado: 0
+        }
+
+        let res = await fetch("http://localhost:5108/calculos", {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            // mode: "no-cors", // no-cors, *cors, same-origin
+            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            // credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+            },
+            // redirect: "follow", // manual, *follow, error
+            // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+
+        let resultado = await res.json();
+
+        console.log(resultado)
 
 
-    //const re = (Number(num1.value)+Number(num2.value))
-    fetch("http://localhost:5108/calculos",  {
-        method:"POST",
-        body:{
-            Np:Number(num1.value),
-            Ns:Number(num2.value),
-            Resultado:0
-        },        mode:"no-cors"
-    
-        }).then(res => res.json())
-        .then(data => console.log(data)).catch(error => console.log(error))
+
+        //result.innerText = `La suma es ${re} pee`
+    } catch (err) {
+        console.log(err)
+    }
 
 
 
-    //result.innerText = `La suma es ${re} pee`
-
-} );
+});
 
 console.log(num1);
